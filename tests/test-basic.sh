9f8f35f4d788f4cbf8599279101e17c81eecc08f
Gunther Klessinger <gunther.klessinger@axiros.com>
2023-09-28 01:20:12 +0200

    ci

--- tests/test-basic.sh
+++ tests/test-basic.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
+set -eu
 
 case "$(uname)" in Linux) OS=linux ;; Darwin) OS=darwin ;; *) OS= ;; esac
 
 function test_linux_or_osx_base {
-set -eu
     D_HOME="/home/"
     test "$OS" = darwin && D_HOME='/Users/'
     test $(whoami) = 'root' && D_HOME='/root/'
-    ls /asdf
-    ./gear asdfasd
     ./gear up b a mm
     source "$HOME/.gears"
     ./gear e gdu
@@ -27,9 +25,7 @@ set -eu
 
 main() {
     test "${1:-}" = "-pdb" && { export GEAR_NO_EXIT_AT_ERR=true && shift; }
-    "${1:-test_linux_or_osx_base}" && return 0
-    ${GEAR_NO_EXIT_AT_ERR:-false} && /bin/bash
-    return 1
+    "${1:-test_linux_or_osx_base}"
 }
 
 main "$@"
